b84f641b0b3b811956212be2dcf1d9ac391a5961
Arom Burgos <gonzalesburgosaromsadith2004@gmail.com>
2023-03-27 22:32:47 -0500

    terminando la pagina p´rincipal del proyecto de musica

--- js/index.js
+++ js/index.js
@@ -19,27 +19,41 @@ let body = document.getElementById("body");
 
     if (body.classList.contains("modoDark")) {
         body.classList.remove("modoDark");
-        modoClaro.innerHTML = "<span class='material-symbols-outlined'>light_mode  Modo Claro</span>"
+        modoClaro.innerHTML = "<span class='material-symbols-outlined'>light_modeModo Claro</span>"
     } 
     else {
         body.classList.add("modoDark");
-        modoClaro.innerHTML = "<span class='material-symbols-outlined'>dark_mode Modo Oscuro</span>"
+        modoClaro.innerHTML = "<span class='material-symbols-outlined'>dark_modeModo Oscuro</span>"
 
     }
     
 }
-modoClaro.addEventListener("click", cambiarModo)
+modoClaro.addEventListener("click", cambiarModo);
 
 
 /* boton musica */
 
 let botonMusica = document.getElementById("botonMusica");
 
+let audio = document.getElementById("audio");
+
+
 function activarMusica(){
-    if(Audio.play()){
-        alert("hola")
+    if(audio.paused){
+        audio.play()
+        botonMusica.innerHTML = "<span  class='material-symbols-outlined'>equalizer</span>";
     }else{
-        alert("hola")
+        audio.pause()
+        botonMusica.innerHTML = "<span  class='material-symbols-outlined'>equalizer</span>";
+    }
+
+    if(botonMusica.classList.contains("animacion_musica")){
+        botonMusica.classList.remove("animacion_musica")
+    }
+    else{
+        botonMusica.classList.add("animacion_musica")
     }
 }
 botonMusica.addEventListener("click", activarMusica)
+
+
